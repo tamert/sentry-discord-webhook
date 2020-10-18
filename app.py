75756143e2f1efff25cff41e727ab89523323b7f
Tamer Ağaoğlu <farerock@gmail.com>
2020-10-18 13:07:27 +0300

    updated message methods

--- app.py
+++ app.py
@@ -19,15 +19,32 @@ migrate = Migrate(app, db)
 class Channel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+    slug = db.Column(db.String(80))
     webhook = db.Column(db.String(300))
     count = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
     active = db.Column(db.Boolean)
+
+
+# class Issue(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(255))
+#     url = db.Column(db.String(255))
+#     location = db.Column(db.String(800))
+#     created_at = db.Column(db.Integer)
+#     channel_id = db.Column(db.Integer)
 
 
 @app.route("/")
 def index():
     channels = Channel.query.all()
     return render_template("index.html", channels=channels, url=request.url_root)
+
+
+@app.route("/detail/<string:id>")
+def detail(id):
+    channel = Channel.query.filter_by(id=id).first()
+    return render_template("detail.html", channel=channel, url=request.url_root)
 
 
 @app.route("/add", methods=["POST"])
@@ -37,19 +54,29 @@ def add_channel():
     new_channel = Channel(name=name, webhook=webhook, count=0, active=True)
     db.session.add(new_channel)
     db.session.commit()
-    return redirect(url_for("index"))
+
+    return redirect(url_for("detail", id=new_channel.id))
 
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
+@app.route("/webhook/<string:id>", methods=["POST"])
+def webhook(id):
     project_name = request.json.get("project_name")
     message = request.json.get("message")
     event = request.json.get("event")
     url = request.json.get("url")
-    channel = Channel.query.filter_by(name=project_name).first()
-    if channel:
+    channel = Channel.query.filter_by(id=id).first()
+    title = project_name + " - " + message if message else "Issue"
+    if channel and project_name:
         channel.count = 1 if channel.count is None else (channel.count + 1)
         db.session.commit()
+
+        # new_issue = Issue(title=event["title"],
+        #                     location=event["location"],
+        #                     created_at=int(event["timestamp"]),
+        #                     channel_id=channel)
+        #
+        # db.session.add(new_issue)
+        # db.session.commit()
 
         unix_timestamp = float(event["timestamp"])
         local_timezone = tzlocal.get_localzone()  # get pytz timezone
@@ -58,7 +85,7 @@ def webhook():
                             json={"username": "sentry",
                                   "embeds": [
                                       {
-                                          "title": message if message else "Issue",
+                                          "title": title,
                                           "description": event["title"],
                                           "fields": [
                                               {
@@ -112,6 +139,17 @@ def active_channel(id):
     channel.active = not channel.active
     db.session.commit()
     return redirect(url_for("index"))
+
+
+@app.route("/edit/<string:id>", methods=["POST"])
+def edit(id):
+    channel = Channel.query.filter_by(id=id).first()
+    name = request.form.get("name")
+    webhook = request.form.get("webhook")
+    channel.name = name
+    channel.webhook = webhook
+    db.session.commit()
+    return redirect(url_for("detail", id=channel.id))
 
 
 @app.route("/delete/<string:id>")
